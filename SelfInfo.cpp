ff56e077ab401d462d6db32621c53d392048c3ef
Muddassir Momin <mominmuddassir01@gmail.com>
2025-01-20 11:02:03 +0530

    Add files via upload

---
+++ SelfInfo.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"My Self Muddassir.\nSecond Year Computer Science & Engineering Student at Dr.JJMCOE\nCurrently I have 8 Months of Experience as an Intern at Orobot Tech Pvt. Ltd."<<endl;
+}
